0e7332b366e4c2d841ea863eb5048a5aad82556a
olehkl1apko <kliapko.oleh.dev@gmail.com>
2024-04-16 18:23:52 +0300

    ADD: routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, lazy } from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
 import { connectWithWebSocket } from "./utils/wssConnection";
+const DashboardPage = lazy(() => import("./pages/dashboardPage"));
+const LoginPage = lazy(() => import("./pages/loginPage"));
 
 function App() {
   useEffect(() => {
     connectWithWebSocket();
   }, []);
 
-  return <main>Hello</main>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
