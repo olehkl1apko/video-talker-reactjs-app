2fda47935167331970f4878fffa3687f366f0f57
olehkl1apko <kliapko.oleh.dev@gmail.com>
2024-04-18 14:04:02 +0300

    UPDATE: calling dialog cmp

--- src/components/DirectCall/CallingDialog/index.jsx
+++ src/components/DirectCall/CallingDialog/index.jsx
@@ -1,9 +1,15 @@
+import { MdCallEnd } from "react-icons/md";
+
 import "./styles.css";
+import { hangUp } from "@/utils/webRTC/webRTCHandler";
 
 export const CallingDialog = () => {
   return (
     <div className="direct_calling_dialog background_secondary_color">
       <span>Calling</span>
+      <div className="dialog_button_container" onClick={() => hangUp()}>
+        <MdCallEnd className="dialog_icon" />
+      </div>
     </div>
   );
 };
--- src/components/DirectCall/RemoteVideoView/index.jsx
+++ src/components/DirectCall/RemoteVideoView/index.jsx
@@ -1,9 +1,12 @@
 import { useRef, useEffect } from "react";
 
 import "./styles.css";
+import { selectCallerUsername } from "@/store/selectors";
+import { useSelector } from "react-redux";
 
 export const RemoteVideoView = ({ remoteStream }) => {
   const remoteVideoRef = useRef();
+  const callerUsername = useSelector(selectCallerUsername);
 
   useEffect(() => {
     if (remoteStream) {
@@ -17,8 +20,9 @@ export const RemoteVideoView = ({ remoteStream }) => {
   }, [remoteStream]);
 
   return (
-    <div className="remote_container">
+    <div className="background_secondary_color remote_container">
       <video className="remote_element" ref={remoteVideoRef} autoPlay />
+      <p>User: {callerUsername}</p>
     </div>
   );
 };
